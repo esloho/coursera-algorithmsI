480a636604fc019a7e75f1e6a782c4a4ca0d2092
esloho <esloho@gmail.com>
2014-10-22 22:33:07 +0200

    Tests added (skeleton)

---
+++ 8PuzzleAssignment/8Puzzle/test/BoardTest.java
@@ -0,0 +1,95 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author esloho
+ *
+ */
+public class BoardTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * Test method for {@link Board#Board(int[][])}.
+     */
+    @Test
+    public final void testBoard() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link Board#dimension()}.
+     */
+    @Test
+    public final void testDimension() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link Board#hamming()}.
+     */
+    @Test
+    public final void testHamming() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link Board#manhattan()}.
+     */
+    @Test
+    public final void testManhattan() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link Board#isGoal()}.
+     */
+    @Test
+    public final void testIsGoal() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link Board#twin()}.
+     */
+    @Test
+    public final void testTwin() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link Board#equals(java.lang.Object)}.
+     */
+    @Test
+    public final void testEqualsObject() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link Board#neighbors()}.
+     */
+    @Test
+    public final void testNeighbors() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link Board#toString()}.
+     */
+    @Test
+    public final void testToString() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
---
+++ 8PuzzleAssignment/8Puzzle/test/SolverTest.java
@@ -0,0 +1,55 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author esloho
+ *
+ */
+public class SolverTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * Test method for {@link Solver#Solver(Board)}.
+     */
+    @Test
+    public final void testSolver() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link Solver#isSolvable()}.
+     */
+    @Test
+    public final void testIsSolvable() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link Solver#moves()}.
+     */
+    @Test
+    public final void testMoves() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link Solver#solution()}.
+     */
+    @Test
+    public final void testSolution() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
